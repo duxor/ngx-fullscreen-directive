9ab3359f8d817f128744d5b57c39d850f50dd9d9
ChepteaCatalin <cheptea.catalin@mail.ru>
2022-08-17 17:24:47 +0300

    add possibility to set custom styles for full screen container

--- src/lib/ngx-fullscreen.directive.ts
+++ src/lib/ngx-fullscreen.directive.ts
@@ -3,6 +3,7 @@ import {
   Directive,
   ElementRef,
   Inject,
+  Input,
   NgModule,
   OnDestroy,
 } from "@angular/core";
@@ -12,13 +13,22 @@ import {
   exportAs: "fullScreen",
 })
 export class FullScreenDirective implements OnDestroy {
-  private _nativeEl = this._el.nativeElement;
-  private _prevStyles;
+  /** Styles applied to the container which has the `fullScreen` directive.
+   *
+   *  These styles are applied only in the full screen mode and are roll-backed when exiting it.
+   */
+  @Input() set fullScreenCtrStyle(style: CSSStyleDeclaration) {
+    this._fullScreenCtrStyle = style;
+  }
+
+  private _nativeEl;
+  private _fullScreenCtrStyle: Partial<CSSStyleDeclaration>;
+  private _prevStyle: Partial<CSSStyleDeclaration>;
   private _listener = [
     "fullscreenchange",
     () => {
       if (!this._document.fullscreenElement) {
-        this._resetFullScreenStyles();
+        this._resetFullScreenStyle();
       }
     },
   ];
@@ -27,52 +37,30 @@ export class FullScreenDirective implements OnDestroy {
     private _el: ElementRef,
     @Inject(DOCUMENT) private _document: Document
   ) {
+    this._nativeEl = this._el.nativeElement;
     this._nativeEl.addEventListener(...this._listener);
   }
 
   openFullScreen = () => {
     if (this._nativeEl.requestFullscreen) {
       this._nativeEl.requestFullscreen();
-      this._addFullScreenStyles();
+      this._addFullScreenStyle();
     }
   };
 
-  private _addFullScreenStyles() {
-    const _style = this._nativeEl.style;
-
-    this._cacheStyles(_style);
-
-    _style.backgroundColor = "white";
-    _style.display = "flex";
-    _style.alignItems = "center";
-    _style.justifyContent = "center";
-    _style.padding = "3rem";
+  private _addFullScreenStyle() {
+    this._prevStyle = { ...this._nativeEl.style };
+    this._asignNativeElStyle(this._fullScreenCtrStyle);
   }
 
-  private _resetFullScreenStyles() {
-    const _style = this._nativeEl.style;
-
-    _style.display = this._prevStyles.display;
-    _style.alignItems = this._prevStyles.alignItems;
-    _style.justifyContent = this._prevStyles.justifyContent;
-    _style.backgroundColor = this._prevStyles.backgroundColor;
-    _style.padding = this._prevStyles.padding;
+  private _resetFullScreenStyle() {
+    this._asignNativeElStyle(this._prevStyle);
   }
 
-  private _cacheStyles({
-    display,
-    alignItems,
-    justifyContent,
-    backgroundColor,
-    padding,
-  }) {
-    this._prevStyles = {
-      display,
-      alignItems,
-      justifyContent,
-      backgroundColor,
-      padding,
-    };
+  private _asignNativeElStyle(style: Partial<CSSStyleDeclaration>) {
+    Object.keys(this._fullScreenCtrStyle).forEach((key) => {
+      this._nativeEl.style[key] = style[key];
+    });
   }
 
   ngOnDestroy(): void {
